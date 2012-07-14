df6e8a5b19679c646a0c5adaad6d7238b0c37cdc
ccheng <ccheng@cchengs-MacBook-Air.local>
2012-07-14 18:43:10 +0800

    mod

--- extract_launched_app.py
+++ extract_launched_app.py
@@ -1,14 +1,18 @@
 from com.android.monkeyrunner import MonkeyDevice, MonkeyRunner
 import re
+import time
 
 device = MonkeyRunner.waitForConnection()
-out = device.shell("logcat -d -s ActivityManager")
-lines = out.split("\n")
+while True:
+    out = device.shell("logcat -d -s ActivityManager && logcat -c")
+    lines = out.split("\n")
 
-cmps = []
-for i, line in zip(range(len(lines)), lines):
-    if line.find("android.intent.category.LAUNCHER") != -1:
-        cmps.append(line)
+    cmps = []
+    for i, line in zip(range(len(lines)), lines):
+        if line.find("android.intent.category.LAUNCHER") != -1:
+            cmps.append(line)
 
-for cmp in cmps:
-    print "package:", "".join(re.findall("cmp=([^/]*)/", cmp)[0])
+    for cmp in cmps:
+        print "package:", "".join(re.findall("cmp=([^/]*)/", cmp)[0])
+
+    time.sleep(3)
