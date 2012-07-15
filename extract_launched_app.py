2be8188f9a89c4a64114f36defaadef006c32fec
ccheng <ccheng@cchengs-MacBook-Air.local>
2012-07-15 09:52:09 +0800

    op

--- extract_launched_app.py
+++ extract_launched_app.py
@@ -1,18 +1,37 @@
-from com.android.monkeyrunner import MonkeyDevice, MonkeyRunner
+import subprocess as sub
 import re
 import time
+import os
 
-device = MonkeyRunner.waitForConnection()
+#Read configuration.
+ips = []
+file = open("config", "r")
+line = file.readline()
+while line != "":
+    line = line.rstrip() + ":5555"
+    ips.append(line)
+    line = file.readline()
+
+#Adb connect
+for ip in ips:
+    os.system("adb connect " + ip)
+
+outfiles = []
+for ip in ips:
+    outfiles.append(open(ip, "a"))
+
 while True:
-    out = device.shell("logcat -d -s ActivityManager && logcat -c")
-    lines = out.split("\n")
+    for ip, outfile in zip(ips, outfiles):
+        args = ("adb -s " + ip + " logcat -d && adb -s " + ip + " logcat -c").split()
+        p = sub.Popen(args ,stdout=sub.PIPE ,stderr=sub.PIPE)
+        out, error = p.communicate()
 
-    cmps = []
-    for i, line in zip(range(len(lines)), lines):
-        if line.find("android.intent.category.LAUNCHER") != -1:
-            cmps.append(line)
-
-    for cmp in cmps:
-        print "package:", "".join(re.findall("cmp=([^/]*)/", cmp)[0])
+        lines = out.split("\n")
+        cmps = []
+        for i, line in zip(range(len(lines)), lines):
+            if line.find("android.intent.category.LAUNCHER") != -1:
+                cmps.append(line)
+        for cmp in cmps:
+            outfile.write("package:" +  "".join(re.findall("cmp=([^/]*)/", cmp)[0]) + "\n")
 
     time.sleep(3)
